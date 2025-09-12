d324604cdb68c34d16a4b409f4c9f8675ef0e2fe
Virnest <dragons12lay@gmail.com>
2025-09-12 17:02:05 +0500

    e

---
+++ install.sh
@@ -0,0 +1,16 @@
+cd home-usr
+
+cp -rv ./ ~/
+
+if [ -d ~/yay ]; then
+   echo 'yay exists'
+else
+    echo "yay dosn't exist"
+    cd ~/
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+fi
+
+cd ..
